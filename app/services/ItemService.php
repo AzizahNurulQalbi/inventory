df2e456f8fbf7b210f05296b4fdafab7a4fee008
Azizah Nurul Qalbi Syahrir <azisahnurul819@gmail.com>
2026-06-29 12:12:17 +0700

    Add testing, logging, debug, security

--- app/services/ItemService.php
+++ app/services/ItemService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Item;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Log;
 
 class ItemService {
     public function all(): Collection {
@@ -15,18 +16,28 @@ class ItemService {
     }
 
     public function create(array $data): Item {
+         Log::info('Item created', [
+    'name' => $data['name']
+]);
         return Item::create($data); 
+       
     }
 
     public function update(int $id, array $data): Item 
     {
         $item = Item::findOrFail($id); 
         $item->update($data); 
+        Log::info('Item updated', [
+    'id' => $id
+]);
         return $item; 
     }
 
     public function delete(int $id): void 
     {
         Item::destroy($id); 
+        Log::info('Item deleted', [
+    'id' => $id
+]);
     }
 }
